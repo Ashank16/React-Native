22809819daeb2346c8fce4427300dab7a6fac39e
Ashank16 <ashankjuyal@gmail.com>
2020-09-10 00:16:31 +0530

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { Card, Text } from 'react-native-elements';
+import { Card, Text, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
+    
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern: '
+        });
+    };
     
     render() {
         return (
@@ -13,7 +22,13 @@ class Contact extends Component {
                     <Text>HONG KONG {"\n"}</Text>
                     <Text>Tel: +852 1234 5678 {"\n"}</Text>
                     <Text>Fax: +852 8765 4321 {"\n"}</Text>
-                    <Text>Email:confusion@food.net {"\n"}</Text>                
+                    <Text>Email:confusion@food.net {"\n"}</Text>  
+                    <Button
+                        title="  Send Email"
+                        buttonStyle={{backgroundColor: "#512DA8"}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.sendMail}
+                    />
                 </Card>
             </Animatable.View>
         );
